e92c4870dfaa3c6c4f5ba7e8d4eb277c4f43b0c5
Antonio Busuladzich <antonio.busuladzich@gmail.com>
2022-12-19 23:34:29 +0200

    Game logic

--- Assets/game.py
+++ Assets/game.py
@@ -1,37 +1,33 @@
 import sys
 import pygame
-from Assets.camera import CameraGroup
-from Assets.tile import Tile, TILE_SIZE
-from Assets.Scene.base_scene import MAP_LAYOUT
 from Assets.settings import SCREEN_WIDTH, SCREEN_HEIGHT
-from Assets.UserInterface.ui import UI
+from Assets.map import Map
+from Assets.Tiles.groups import *
 
+# Generate the Map before the pygame windows is started
 pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("Age of Empyres")
+pygame.display.set_icon(pygame.image.load("Assets/Textures/grail.png"))
+camera = CameraGroup()
+ui_group = UIGroup()
+
+world = Map(camera)
 
 clock = pygame.time.Clock()
-camera = CameraGroup()
-ui_group = pygame.sprite.Group()
+camera.custom_draw()
 
-for row_index, row in enumerate(MAP_LAYOUT):
-    for col_index, col in enumerate(row):
-        x = col_index * TILE_SIZE;
-        y = row_index * TILE_SIZE;
-        Tile(pos=(x, y), group=camera, tile_type="sand")
-
-ui = UI(ui_group)
 
 while True:
-    print(pygame.mouse.get_pos())
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
-    screen.fill((0, 204, 0))
-
-    camera.update()
-    camera.custom_draw()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:  # Turn on/off game pause
+            ui_group.set_pause()
+    if not ui_group.isPaused:
+        screen.fill((0, 204, 0))
+        camera.update()
+        camera.custom_draw()
     ui_group.update()
     ui_group.draw(screen)
     pygame.display.update()

9b5cc3191d7aeda263d75494639d22460a167c55
Antonio Busuladzich <antonio.busuladzich@gmail.com>
2022-12-19 23:34:18 +0200

    settings update

--- Assets/settings.py
+++ Assets/settings.py
@@ -1,3 +1,15 @@
 # Global vars used across the different python files
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 704
+MAP_SETTINGS = {
+    "Tiles": {
+        "Count": {  # Total Map Dimensions in Tiles Count
+            "x": 120,
+            "y": 120
+        },
+        "Size": {  # Single Tile Dimensions
+            "x": 64,
+            "y": 64
+        }
+    }
+}

ad63bcbbb027d7d6e93fd2fb0d680076373c5e84
Antonio Busuladzich <antonio.busuladzich@gmail.com>
2022-12-19 23:34:09 +0200

    World terrain

--- Assets/map.py
+++ Assets/map.py
@@ -1,4 +1,24 @@
-import Scene.baseScene as baseScene
-import Scene.dirtScene as dirtScene
+from pygame.math import Vector2 as Vector2
+from Assets.settings import MAP_SETTINGS, SCREEN_HEIGHT, SCREEN_WIDTH
+from Assets.tile import Tile
 
-AREAS = [dirtScene.MAP_LAYOUT, baseScene.MAP_LAYOUT, dirtScene.MAP_LAYOUT]
+
+class Map:
+    def __init__(self, group):
+        self.__MAP_SIZE = Vector2((MAP_SETTINGS["Tiles"]["Count"]["x"],
+                                   MAP_SETTINGS["Tiles"]["Count"]["y"]))
+        
+        self.__TILE_SIZE = Vector2((MAP_SETTINGS["Tiles"]["Size"]["x"],
+                                    MAP_SETTINGS["Tiles"]["Size"]["y"]))
+        self.__generate_terrain(group)
+        
+    def __generate_terrain(self, group) -> list:
+        total_map_size = self.__MAP_SIZE.elementwise()*self.__TILE_SIZE
+        counter: int = 0
+        for row in range(-int(total_map_size.y/2), int(total_map_size.y/2), 64):
+            for col in range(-int(total_map_size.x/2), int(total_map_size.x/2), 64):
+                Tile((col, row), group, "grass")
+
+    def __generate_resources(self):
+        return
+        # TO DO
--- Assets/tile.py
+++ Assets/tile.py
@@ -1,12 +1,15 @@
 import pygame
-TILE_SIZE = 64
+from pygame.math import Vector2 as Vector2
+from Assets.settings import MAP_SETTINGS
 
 
 class Tile(pygame.sprite.Sprite):
     TILE_VARIATIONS = ['sand', 'dirt']
 
-    def __init__(self, pos, group, tile_type, size=(TILE_SIZE, TILE_SIZE)):
+    def __init__(self, pos, group, tile_type):
         super().__init__(group)
+        self.TILE_SIZE = Vector2((MAP_SETTINGS["Tiles"]["Size"]["x"],
+                                  MAP_SETTINGS["Tiles"]["Size"]["y"]))
         self.image = pygame.image.load(f"Assets/Textures/{tile_type}.png")
-        self.image = pygame.transform.scale(self.image, size)
+        self.image = pygame.transform.scale(self.image, self.TILE_SIZE)
         self.rect = self.image.get_rect(topleft=pos)

dbdbb6226db11bf9fab64705d8387dba1c78cea4
Antonio Busuladzich <antonio.busuladzich@gmail.com>
2023-01-01 15:19:09 +0200

    Resource abstraction and selection

--- Assets/tile.py
+++ Assets/tile.py
@@ -8,8 +8,21 @@ class Tile(pygame.sprite.Sprite):
 
     def __init__(self, pos, group=pygame.sprite.AbstractGroup(), tile_type=""):
         super().__init__(group)
+        self.pos = pos
+        self.group = group
         self.TILE_SIZE = Vector2((MAP_SETTINGS["Tiles"]["Size"]["x"],
                                   MAP_SETTINGS["Tiles"]["Size"]["y"]))
-        self.image = pygame.image.load(f"Assets/Textures/{tile_type}.png")
+        self.tile_type = tile_type
+        self.image = pygame.image.load(f"Assets/Textures/{self.tile_type}.png")
         self.image = pygame.transform.scale(self.image, self.TILE_SIZE)
-        self.rect = self.image.get_rect(topleft=pos)
+        self.rect = self.image.get_rect(topleft=self.pos)
+        self.border: pygame.Rect = None
+
+    def draw_border(self):
+        self.border = pygame.draw.rect(surface=self.image,
+                                       color=(255, 255, 255),
+                                       rect=[0, 0, self.TILE_SIZE.x, self.TILE_SIZE.y],
+                                       width=1)
+
+    def delete_border(self):
+        self.image = pygame.image.load(f"Assets/Textures/{self.tile_type}.png")
